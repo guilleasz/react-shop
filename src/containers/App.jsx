3b4e6abb0022563a35d338b285b5cd753511d0cd
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-11-10 12:23:49 -0300

    added loading

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,25 +7,30 @@ import { type Product } from '../types';
 type Props = {};
 
 type State = {
-  products: Product[]
+  products: Product[],
+  loading: boolean
 }
 
 export default class App extends React.Component<Props, State> {
   state = {
     products: [],
+    loading: true,
   }
 
   componentDidMount() {
     axios.get('/products')
       .then((res: $AxiosXHR<Product[]>): Product[] => res.data)
-      .then((products: Product[]) => this.setState({ products }));
+      .then((products: Product[]) => this.setState({ products, loading: false }));
   }
 
   render() {
     return (
-      <div>
-        <Grid products={this.state.products} />
-      </div>
+      this.state.loading ?
+        <div>Loading</div>
+        :
+        <div>
+          <Grid products={this.state.products} />
+        </div>
     );
   }
 }
